17b552c7db99c55bdeb285b3c4c743165ec40714
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-03-20 22:05:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,4 @@ module.exports = gitterChat
 
 
 
-// the following put html or js : both success
-function makeGitterURL (username) {
-  return `https://gitter.im/${username}/~embed`
-}
-	
-	    
-var exampleProfile = {
-  name: 'nina',
-  username: 'ninabreznik',
-  photo:'https://nomadlist.com/assets/img/cities/phuket-thailand-500px.jpg'
-}
+
